070cf7d01f5478ffdf97a180a991ee44df658910
Shridhar Bagal <shridharbagal345@gmail.com>
2025-01-17 00:53:10 +0530

    initial commit

---
+++ src/main/java/com/chatbot/peru/PeruApplication.java
@@ -0,0 +1,13 @@
+package com.chatbot.peru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeruApplication.class, args);
+	}
+
+}
